0f744199434920480b17bec2309215afea56bf2e
arik <lewin.arik@gmail.com>
2013-08-28 18:04:02 +0300

    cat installer added with support for bower and grunt

---
+++ catinstall.js
@@ -0,0 +1,89 @@
+var os = require('os'),
+    fs = require("fs"),
+    spawn,
+    options = {
+        cachePassword: true,
+        prompt: 'Additional global npm(s) about to be installed, set your root password:? ',
+        spawnOptions: { /* other options for spawn */ }
+    },
+    process,
+    commands = [
+        {
+            command: "npm",
+            args: ["install", "-g", "grunt-cli"]
+        },
+        {
+            command: "npm",
+            args: ["install", "-g", "bower"]
+        }
+    ], next= 0, size = commands.length;
+
+function isLinux() {
+    return (os.platform() == "linux");
+}
+
+function init() {
+    var log = "CAT installer Initialized ....";
+    console.log(log);
+    log2file(log);
+
+    if (isLinux()) {
+        spawn = require('sudo');
+    } else {
+        spawn = require('child_process');
+    }
+
+}
+
+function log2file(data, option) {
+    try {
+        fs.appendFileSync("catinstaller.log", data, "utf8");
+    } catch (e) {
+        log2file("Cat Installer, ERROR:", e);
+    }
+}
+
+
+function install(items) {
+
+    var item = items[next],
+        command = item.command,
+        args = item.args,
+        print;
+
+    if (isLinux()) {
+        args.unshift(command);
+        print = args.join(" ");
+        console.log("Running Installer command: " + print);
+        process = spawn(args, options);
+
+    } else {
+        print = [command, args.join(" ")].join(" ");
+        console.log("Running Installer command: " + print);
+        process = spawn(command, args);
+
+    }
+
+
+    process.stdout.on('data', function (data) {
+        var log = 'CAT Installer: ' + data;
+        console.log(log);
+        log2file(log);
+    });
+
+    process.stderr.on('data', function (data) {
+        log2file('CAT Installer, ERROR: ' + data);
+    });
+
+    process.on('close', function (code) {
+        log2file('CAT Installer Complete ' + code);
+        next++;
+        if (next < size) {
+            install(items);
+        }
+    });
+}
+
+
+init();
+install(commands);
