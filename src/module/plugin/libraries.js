9b8a791af987852dbc994f824adaa430c43d6dec
Arik Lewin <lastboy@Ariks-MacBook-Pro.local>
2013-10-28 14:33:30 +0200

    clean whitespaces

--- src/module/plugin/libraries.js
+++ src/module/plugin/libraries.js
@@ -16,7 +16,8 @@ module.exports = _basePlugin.ext(function () {
         _data,
         _internalConfig,
         _project,
-        _me = this;
+        _me = this,
+        concatenated = [];
 
     return {
 
@@ -38,12 +39,14 @@ module.exports = _basePlugin.ext(function () {
                 manifestFileName = "manifest.json",
                 manifestLib = _path.join(global.catlibs, manifestFileName),
                 catProjectLib,
+                catProjectLibName,
                 library, mode,
                 workPath,
                 libWorkPath,
                 manifest,
                 libraries,
-                slot = 0;
+                slot = 0,
+                envinfo;
 
             if (_fs.existsSync(manifestLib)) {
                 manifest = _fs.readFileSync(manifestLib, "utf8");
@@ -68,6 +71,8 @@ module.exports = _basePlugin.ext(function () {
                  * @private
                  */
                 function _copy(base, item) {
+                    var content;
+
                     // copy the library to the current cat project
                     try {
                         // copy dev
@@ -77,6 +82,13 @@ module.exports = _basePlugin.ext(function () {
                             to = _path.join(catProjectLib, item);
                             _utils.copySync(from, to);
 
+                            // concatenation
+                            content = _fs.readFileSync(from);
+                            if (content) {
+                                concatenated.push(content);
+                            }
+
+
                         } else {
                             _log.warning(_props.get("cat.plugin.libraries.config.missing").format("[libraries ext]", library.name));
                         }
@@ -85,9 +97,9 @@ module.exports = _basePlugin.ext(function () {
                     }
                 }
 
-                // copy artifacts
                 if (artifact && _typedas.isArray(artifact)) {
                     artifact.forEach(function (item) {
+                        // copy artifacts
                         _copy((library.base || undefined), item);
                     });
                 }
@@ -246,9 +258,16 @@ module.exports = _basePlugin.ext(function () {
 
                 slot++;
                 if (slot > libraries.length) {
+
+                    // concat artifact files
+                    if (concatenated.length > 0) {
+                        _fs.writeFileSync(_path.join(catProjectLib, catProjectLibName), concatenated.join(""))
+                    }
+
                     if (_emitter) {
                         _emitter.emit("job.done", {status: "done"});
                     }
+
                     return undefined;
                 }
 
@@ -285,10 +304,22 @@ module.exports = _basePlugin.ext(function () {
 
                     manifest = JSON.parse(manifest);
 
-                    catProjectLib = (_project ? _project.getInfo("lib.source") : undefined);
+
                     libraries = manifest.libraries;
                     mode = manifest.mode;
-                    workPath = _path.join(cathome, _project.getInfo("libraries").path);
+
+
+                    if (_project) {
+                        catProjectLib = _project.getInfo("lib.source");
+                        envinfo = _project.getInfo("env");
+                        if (envinfo) {
+                            catProjectLibName = (envinfo.lib ? envinfo.lib.name : undefined);
+                        }
+                        if (!catProjectLibName) {
+                            _log.info('[library] No valid name was found for CAT library (see catproject)');
+                        }
+                        workPath = _path.join(cathome, _project.getInfo("libraries").path);
+                    }
 
                     if (libraries &&
                         _typedas.isArray(libraries)) {
