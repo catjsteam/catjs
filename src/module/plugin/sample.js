5140999274ecb515f70f63c8375ef8cf8e725d93
Arik Lewin <lastboy@Ariks-MacBook-Pro.local>
2013-11-03 09:50:04 +0200

    sample plugin

---
+++ src/module/plugin/sample.js
@@ -0,0 +1,71 @@
+var _catglobal = catrequire("cat.global"),
+    _log = _catglobal.log(),
+    _path = require("path"),
+    _props = catrequire("cat.props"),
+    _basePlugin = catrequire("cat.plugin.base"),
+    _utils = catrequire("cat.utils"),
+    _spawn = catrequire("cat.plugin.spawn"),
+    _fs = require("fs.extra"),
+    _typedas = require("typedas"),
+    _bower = require('bower');
+
+module.exports = _basePlugin.ext(function () {
+
+    var _emitter,
+        _global,
+        _data,
+        _internalConfig,
+        _project,
+        _me = this;
+
+    return {
+
+        /**
+         *  Initial plugin function
+         *
+         * @param config The configuration:
+         *          data - The configuration data
+         *          emitter - The emitter reference
+         *          global - The global data configuration
+         *          internalConfig - CAT internal configuration
+         */
+        init: function (config) {
+
+            var customAttribute,
+                extensionParams,
+                errors = ["[libraries plugin] No valid configuration"];
+
+            if (!config) {
+                _log.error(errors[1]);
+                _me.setDisabled(true);
+            }
+
+            _emitter = config.emitter;
+            _global = config.global;
+            _data = config.data;
+            _internalConfig = config.internalConfig;
+            _project = (_internalConfig ? _internalConfig.getProject() : undefined);
+
+            // initial data binding to 'this'
+            _me.dataInit(_data);
+            extensionParams = _data.data;
+
+            customAttribute = extensionParams.customAttribute;
+
+            if (config && extensionParams) {
+
+                // Your code in here ...
+
+                console.log("[sample test Init] an external attributes named, customAttribute='" + customAttribute + "'");
+                console.log("[sample test init] Project: ", JSON.stringify(_project));
+
+
+                // done processing notification for the next task to take place
+                _me.getEmitter().emit("job.done", {status: "done"});
+
+            }
+        }
+
+    }
+
+});
