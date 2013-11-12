9fbd4402bdbe33f32bc7a2a55209bce0e1ee0c8f
Pablo Retyk <pablo@ubuntu.(none)>
2013-11-12 11:27:08 +0200

    Dummy commit

--- src/libraries/cat/core/Ext/ChaiUtils.js
+++ src/libraries/cat/core/Ext/ChaiUtils.js
@@ -32,7 +32,7 @@ _cat.utils.chai = function () {
 
         var config  = _cat.core.getConfig();
 
-        var url = "http://" + config.ip +  ":" + 
+        var url = "http://" + config.ip +  ":" +
             config.port + "/assert?testName=" +
             name + "&message=" + message +
             "&status=" + status +
@@ -42,7 +42,7 @@ _cat.utils.chai = function () {
     }
 
     return {
-
+ 
         assert: function (config) {
 
             if (!_state) {
