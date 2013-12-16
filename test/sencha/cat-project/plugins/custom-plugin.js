b36d6ec1bf61ea78a22f0ccb6287194295043089
arik <lewin.arik@gmail.com>
2013-12-16 16:12:12 +0200

    Internal catproject.json support with custom plugin sample embedded with sencha test app

---
+++ test/sencha/cat-project/plugins/custom-plugin.js
@@ -0,0 +1,47 @@
+var _Scrap = catrequire("cat.common.scrap"),
+    _utils = catrequire("cat.utils");
+
+module.exports = function () {
+
+    return {
+
+        init: function () {
+
+
+            // init scrap plugin code in here
+
+
+            /**
+             * Sample for custom annotation
+             *
+             *  properties:
+             *  name    - sample
+             *  single  - false
+             *  singleton - 1[default -1]
+             *  $type   - js
+             */
+            _Scrap.add({name: "sample",
+                single: false,
+                singleton: 1,
+                func: function (config) {
+                    var sample,
+                        me = this;
+
+                    sample = this.get("sample");
+                    console.log('CAT... Custom annotation sample sample:', sample);
+                    me.print("console.log('CAT... Custom annotation sample sample:', " + sample + ")");
+                }});
+
+        },
+
+        apply: function () {
+            // apply scrap plugin code in here
+
+        },
+
+        getType: function () {
+            return "custom-plugin";
+        }
+    }
+
+};
