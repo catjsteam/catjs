9e412957e66b120b72eceaea80b254cddfab72d5
arik <lewin.arik@gmail.com>
2014-01-22 12:34:52 +0200

    sanbox project alignment with new catproject.json structure

---
+++ test/sandbox/cat-tests/src/test/test.js
@@ -0,0 +1,23 @@
+var test = function() {
+
+    return {
+
+        init: function() {
+
+            /*
+                @[scrap
+                    @@name
+                    @@replace after:1
+                    @@behavior [
+                     @@inject comment
+                     @@replace
+                    ]
+                ]@
+             */
+            console.log("This is a test");
+
+        }
+
+    };
+
+}();
