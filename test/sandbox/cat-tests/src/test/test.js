19f0149bdcd40c5dd55165d21ec35466e02e61f7
arik <lewin.arik@gmail.com>
2014-01-28 18:59:28 +0200

    cat test behavior

--- test/sandbox/cat-tests/src/test/test.js
+++ test/sandbox/cat-tests/src/test/test.js
@@ -6,15 +6,27 @@ var test = function() {
 
             /*
                 @[scrap
-                    @@name test
+                    @@name test1
                     @@replace after:2
                     @@behavior[
-                        @@replace comment
+                        @@replace cat.comment
                     ]
                 ]@
              */
             console.log("This is a test line:1");
             console.log("This is a test line:2");
+
+            /*
+                @[scrap
+                    @@name test2
+                    @@replace after:2
+                    @@behavior[
+                        @@replace src/common/test1.js
+                    ]
+                ]@
+             */
+            console.log("This is a test line:3");
+            console.log("This is a test line:4");
 
         }
 
