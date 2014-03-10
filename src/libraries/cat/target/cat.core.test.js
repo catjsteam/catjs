8974b94152a01bf0b55e90e93d37bf15721ce16c
Ran Snir <Ran.Snir@hp.com>
2014-03-10 10:54:11 +0200

    remove cat-site, update cat.core.test

--- src/libraries/cat/target/cat.core.test.js
+++ src/libraries/cat/target/cat.core.test.js
@@ -534,7 +534,8 @@ _cat.utils.chai = function () {
                         style: ( (testdata.getStatus() === "success") ? "color:green" : "color:red" ),
                         header: testdata.getDisplayName(),
                         desc: testdata.getMessage(),
-                        tips: _cat.core.TestManager.getTestCount()
+                        tips: _cat.core.TestManager.getTestCount(),
+                        elementType : ( (testdata.getStatus() === "success") ? "listImageCheck" : "listImageCross" )
                     });
                     _sendTestResult(testdata);
 
@@ -835,8 +836,8 @@ function TestDB (){
     };
 }
 _cat.core.ui = function () {
-
     function _create() {
+
 
         var catElement;
         if (typeof document !== "undefined") {
@@ -852,10 +853,10 @@ _cat.core.ui = function () {
             catElement.innerHTML = '<div id="cat-status" class="cat-dynamic cat-status-open">' +
                 '<div id=loading></div>' +
                 '<div id="catlogo"></div>' +
-                '<div id="cat-status-content">' +
+                '<div id="catHeader">CAT - Tests</div>' +
                 '<div class="text-tips"></div>' +
-                '<div class="text-top"><span style="color:green"></span></div>' +
-                '<div class="text"></div>' +
+                '<div id="cat-status-content">' +
+                '<ul id="testList"></ul>' +
                 '</div>' +
                 '</div>';
 
@@ -892,7 +893,7 @@ _cat.core.ui = function () {
         if (catElement) {
             catStatusElt = _getCATStatusElt();
             if (catStatusElt) {
-                catStatusContentElt = catStatusElt.childNodes[2];
+                catStatusContentElt = catStatusElt.childNodes[3];
             }
         }
 
@@ -907,6 +908,8 @@ _cat.core.ui = function () {
             reset: true
         });
     }
+
+    var testNumber = 0;
 
     var _me =  {
 
@@ -1039,6 +1042,8 @@ _cat.core.ui = function () {
                 isOpen = false,
                 reset = ("reset" in config ? config.reset : false);
 
+
+
             function _setText(elt, text, style) {
 
                 var styleAttrs = (style ? style.split(";") : []);
@@ -1051,10 +1056,7 @@ _cat.core.ui = function () {
                         }
                     });
 
-                        elt.textContent = text;
-
-
-
+                    elt.textContent = text;
                 }
             }
 
@@ -1076,21 +1078,49 @@ _cat.core.ui = function () {
                                 }, 300);
                             }
                         }
+                        var innerListElement =
 
-                        setTimeout(function() {
+                                '<div class="text-top"><span style="color:green"></span></div>' +
+                                '<div class="text"></div>';
 
-                            if ("header" in config) {
-                                _setText(catStatusContentElt.childNodes[1]  , config.header, config.style);
-                            }
-                            if ("desc" in config) {
-                                _setText(catStatusContentElt.childNodes[2], config.desc, config.style);
+                        if (config.header || config.desc || config.tips) {
+                            var ul = document.getElementById("testList");
+                            var newLI = document.createElement("LI");
+                            ul.insertBefore(newLI, ul.children[0]);
+                            newLI.innerHTML = innerListElement;
 
-                            }
-                            if ("tips" in config) {
-                                _setText(catStatusContentElt.childNodes[0], config.tips, config.style);
-                            }
+                            var textTips =  document.getElementsByClassName("text-tips")[0];
 
-                        }, 300);
+                            setTimeout(function() {
+
+                                // add element to ui test list
+                                if ("header" in config) {
+                                    _setText(newLI.childNodes[0]  , config.header, config.style);
+                                }
+                                if ("desc" in config) {
+                                    _setText(newLI.childNodes[1], config.desc, config.style);
+                                }
+
+                                if ("tips" in config) {
+                                    if (config.tips) {
+                                        testNumber  = config.tips;
+                                        _setText(textTips, "Number of test passed : " + testNumber, config.style);
+                                    } else {
+                                        _setText(textTips, "Number of test passed : " + testNumber, "color : green");
+                                    }
+
+                                }
+
+                                if ("elementType" in config) {
+                                    newLI.className = newLI.className + " " + config.elementType;
+
+                                } else {
+                                    newLI.className = newLI.className + " listImageInfo";
+                                }
+
+                            }, 300);
+                        }
+
                     }
                 }
             }
@@ -1435,11 +1465,6 @@ _cat.plugins.jqm = function () {
             swipePageLeft : function() {
                 $(document).ready(function(){
                     $( ".ui-page-active" ).swipeleft();
-//                    var next = $( ".ui-page-active" ).jqmData( "next" );
-//                    $( ":mobile-pagecontainer" ).pagecontainer( "change", next + ".html", {
-//
-//                    });
-
 
                 });
 
@@ -1449,12 +1474,9 @@ _cat.plugins.jqm = function () {
 
             swipePageRight : function() {
                 $(document).ready(function(){
-//                    $( ".ui-page-active" ).swiperight();
-                    var prev = $( ".ui-page-active" ).jqmData( "prev" );
-                    $( ":mobile-pagecontainer" ).pagecontainer( "change", prev + ".html", {
 
-                        reverse: true
-                    });
+                    var prev = $( ".ui-page-active" ).jqmData( "prev" );
+
                 });
             },
 
@@ -1481,7 +1503,10 @@ _cat.plugins.jqm = function () {
 
             setText : function (idName, value) {
                 $(document).ready(function(){
+                    $("#"+ idName).focus();
                     $("#"+ idName).val(value);
+                    $("#"+ idName).trigger( 'change' );
+                    $("#"+ idName).blur();
                 });
             },
 
@@ -1735,6 +1760,11 @@ _cat.plugins.sencha = function () {
                 list.select(index);
             },
 
+
+            changeView : function (viewName) {
+                var firststep = Ext.create(viewName);
+                Ext.Viewport.setActiveItem(firststep);
+            },
 
             removePanel : function (panelId) {
 
--- test/cat-site/cat-test-app/js/snippets/javascript.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-"use strict";
-
-exports.snippetText = require("../requirejs/text!./javascript-jquery.snippets")
-    + "\n"
-    + require("../requirejs/text!./javascript.snippets");
-exports.scope = "javascript";
-
-});
