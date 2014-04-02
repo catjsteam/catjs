8862d4bd45866dbfc549f855c6b2b8549cfebde5
Ran Snir <Ran.Snir@hp.com>
2014-04-02 15:40:02 +0300

    velocity demo

---
+++ test/velocity/full-demo/cat-project/src/velocity/index.js
@@ -0,0 +1,51 @@
+_cat.core.define("C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.setUsername", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.setUsername";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.setUsername
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.setPassword", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.setPassword";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.setPassword
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
+_cat.core.define("C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.clickLogin", function() {
+
+  var pkgName = "C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.clickLogin";
+
+  return {
+
+    /**
+     * Init functionality for scrap C:.Users.snirr.workspace.CATCore.test.velocity.full-demo.cat-project.target.velocity.index.html.clickLogin
+     *
+     * @param content CAT Context object
+     */
+    init: function(context) {
+
+    }
+  };
+
+}());
