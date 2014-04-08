5fd0220fe6127b6be155927d0c22e2160087968b
arik <lewin.arik@gmail.com>
2014-04-08 15:48:59 +0300

    tests alignment to the new folder structure and build fixes

--- test/sencha/build.js
+++ test/sencha/build.js
@@ -1,2 +1,14 @@
-var build = require("./../testModule.js");
-build.test();
+var args = process.argv.slice(2),
+    build = require("./../testModule.js");
+
+if (args && args.length > 0) {
+    if (args[0] === "all") {
+        build.test();
+    }
+
+} else {
+    process.chdir("./../..");
+    build.run("sencha");
+
+}
+
