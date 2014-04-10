938136c12ddc09cf6a3e56637115d952e3bcaebd
arik <lewin.arik@gmail.com>
2014-04-10 10:30:37 +0300

    catcli file changed to catcli.js - aligned as to the catjs repository

---
+++ catcli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var pjson = require('./package.json');
+
+console.log("[CAT] path: " + __dirname);
+console.log("[CAT] version: " + pjson.version);
+
+require("./src/module/CATCli.js").init({dirname:__dirname});
