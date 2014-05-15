caad183c47f9d9fb5abcc6767d5a92bfbc2170b2
arik <lewin.arik@gmail.com>
2014-05-15 14:11:42 +0300

    fix for issue github-#1

--- src/plugins/scrap-enyo.js
+++ src/plugins/scrap-enyo.js
@@ -27,52 +27,76 @@ module.exports = function () {
                     var enyoRows,
                         enyo,
                         me = this,
-                        validcode = false;
+                        generate = function (enyoRow) {
+
+
+                            if (enyoRows) {
+                                var enyo;
+
+                                enyoRow = _utils.prepareCode(enyoRow);
+
+                                if (enyoRow && enyoRow.join) {
+                                    enyo = enyoRow.join("\n");
+                                } else {
+                                    enyo = enyoRow;
+                                }
+
+                                if (enyo) {
+
+                                    var match;
+
+                                    match = _scraputils.generate({
+                                        api: "waterfall",
+                                        exp: enyo
+                                    });
+
+                                    if (!match) {
+                                        match = _scraputils.generate({
+                                            api: "next",
+                                            exp: enyo
+                                        });
+                                    }
+
+                                    if (!match) {
+                                        match = _scraputils.generate({
+                                            api: "setSelected",
+                                            exp: enyo
+                                        });
+                                    }
+
+
+                                    if (match) {
+                                        me.print(_tplutils.template({
+                                            content: funcSnippetTpl,
+                                            data: {
+                                                comment: " Generated code according to the enyo scrap comment (see @@enyo)",
+                                                code: ("_cat.core.plugin('enyo').actions."+ match)
+                                            }
+                                        }));
+                                    }
+                                }
+                            }
+                        };
+
 
                     enyoRows = this.get("enyo");
+                    var scrapConf = me.config,
+                        scrap = scrapConf,
+                        scrapName;
 
                     if (enyoRows) {
-                        _utils.prepareCode(enyoRows);
-                        enyo = enyoRows.join("\n");
+                        scrap = scrapConf;
+                        scrapName = (scrap.name ? scrap.name[0] : undefined);
 
-                        if (enyo) {
-
-                            var match,
-                                prefix;
-
-                            match = _scraputils.generate({
-                                api: "waterfall",
-                                exp: enyo
-                            });
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "next",
-                                    exp: enyo
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setSelected",
-                                    exp: enyo
-                                });
-                            }
-
-
-                            if (match) {
-                                me.print(_tplutils.template({
-                                    content: funcSnippetTpl,
-                                    data: {
-                                        comment: " Generated code according to the enyo scrap comment (see @@enyo)",
-                                        code: ("_cat.core.plugin('enyo').actions."+ match)
-                                    }
-                                }));
-                            }
-
-
-
+                        if (enyoRows && enyoRows.join) {
+                            me.print("_cat.core.ui.setContent({style: 'color:#0080FF', header: '" + scrapName + "', desc: '" + enyoRows + "',tips: ''});");
                         }
+
+                        enyoRows.forEach(function (enyoRow) {
+                            if (enyoRow) {
+                                generate(enyoRow);
+                            }
+                        });
                     }
                 }
             });
--- src/plugins/scrap-sencha.js
+++ src/plugins/scrap-sencha.js
@@ -24,189 +24,213 @@ module.exports = function () {
                     var senchaRows,
                         sencha,
                         me = this,
-                        validcode = false;
+                        generate = function (senchaRow) {
+
+                            var sencha;
+                            senchaRow = _utils.prepareCode(senchaRow);
+
+                            if (senchaRow && senchaRow.join) {
+                                sencha = senchaRow.join("\n");
+                            } else {
+                                sencha = senchaRow;
+                            }
+
+
+                            if (sencha) {
+
+                                var match = _scraputils.generate({
+                                    api: "tap",
+                                    apiname: "fireTap",
+                                    exp: sencha
+                                });
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "setText",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "tapButton",
+                                        apiname: "tapButton",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "setTextValue",
+                                        apiname: "setTextValue",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "setChecked",
+                                        apiname: "setChecked",
+                                        exp: sencha
+                                    });
+                                }
+
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "setUnchecked",
+                                        apiname: "setUnchecked",
+                                        exp: sencha
+                                    });
+                                }
+
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "setDate",
+                                        apiname: "setDate",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "setSliderValue",
+                                        apiname: "setSliderValue",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "setSliderValues",
+                                        apiname: "setSliderValues",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "setToggle",
+                                        apiname: "setToggle",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "scrollBy",
+                                        apiname: "scrollBy",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "scrollToTop",
+                                        apiname: "scrollToTop",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "carouselNext",
+                                        apiname: "carouselNext",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "carouselPrevious",
+                                        apiname: "carouselPrevious",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "scrollToEnd",
+                                        apiname: "scrollToEnd",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "scrollToListIndex",
+                                        apiname: "scrollToListIndex",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "listSelectIndex",
+                                        apiname: "listSelectIndex",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "changeTab",
+                                        apiname: "changeTab",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "nestedlistSelect",
+                                        apiname: "nestedlistSelect",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "removePanel",
+                                        apiname: "removePanel",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (!match) {
+                                    match = _scraputils.generate({
+                                        api: "nestedlistBack",
+                                        apiname: "nestedlistBack",
+                                        exp: sencha
+                                    });
+                                }
+
+                                if (match) {
+                                    me.print("_cat.core.plugin('sencha').actions." + match);
+                                }
+
+                            }
+                        };
+
 
                     senchaRows = this.get("sencha");
+                    var scrapConf = me.config,
+                        scrap = scrapConf,
+                        scrapName;
 
                     if (senchaRows) {
-                        _utils.prepareCode(senchaRows);
-                        sencha = senchaRows.join("\n");
+                        scrap = scrapConf;
+                        scrapName = (scrap.name ? scrap.name[0] : undefined);
 
-
-                        if (sencha) {
-
-                            var match = _scraputils.generate({
-                                api: "tap",
-                                apiname: "fireTap",
-                                exp: sencha
-                            });
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setText",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "tapButton",
-                                    apiname: "tapButton",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setTextValue",
-                                    apiname: "setTextValue",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setChecked",
-                                    apiname: "setChecked",
-                                    exp: sencha
-                                });
-                            }
-
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setUnchecked",
-                                    apiname: "setUnchecked",
-                                    exp: sencha
-                                });
-                            }
-
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setDate",
-                                    apiname: "setDate",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setSliderValue",
-                                    apiname: "setSliderValue",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setSliderValues",
-                                    apiname: "setSliderValues",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "setToggle",
-                                    apiname: "setToggle",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "scrollBy",
-                                    apiname: "scrollBy",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "scrollToTop",
-                                    apiname: "scrollToTop",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "carouselNext",
-                                    apiname: "carouselNext",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "carouselPrevious",
-                                    apiname: "carouselPrevious",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "scrollToEnd",
-                                    apiname: "scrollToEnd",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "scrollToListIndex",
-                                    apiname: "scrollToListIndex",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "listSelectIndex",
-                                    apiname: "listSelectIndex",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "changeTab",
-                                    apiname: "changeTab",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "nestedlistSelect",
-                                    apiname: "nestedlistSelect",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "removePanel",
-                                    apiname: "removePanel",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (!match) {
-                                match = _scraputils.generate({
-                                    api: "nestedlistBack",
-                                    apiname: "nestedlistBack",
-                                    exp: sencha
-                                });
-                            }
-
-                            if (match) {
-                                me.print("_cat.core.plugin('sencha').actions."+ match);
-                            }
-
+                        if (senchaRows && senchaRows.join) {
+                            me.print("_cat.core.ui.setContent({style: 'color:#0080FF', header: '" + scrapName + "', desc: '" + (senchaRows.split ? senchaRows.split("\n").join("") : senchaRows) + "',tips: ''});");
                         }
+
+                        senchaRows.forEach(function (senchaRow) {
+                            if (senchaRow) {
+                                generate(senchaRow);
+                            }
+                        });
                     }
                 }
             });
