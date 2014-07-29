c804f63375bf887caaa53f94926f9cd09bd1ec50
arik <lewin.arik@gmail.com>
2014-07-29 19:34:03 +0300

    begin test in case the test-apps folder downloaded already - fix

--- test/test.js
+++ test/test.js
@@ -8,16 +8,16 @@ var args = process.argv.slice(2),
     _fs = require("fs"),
     path = _path.resolve("./node_modules"),
     _extractFolder = "test-apps-master"
-    _zipfolder =  "./test/catjs-test-apps",
+_zipfolder = "./test/catjs-test-apps",
     _zipfile = _zipfolder + ".zip",
     _spawn = require('child_process');
 
 
 function _call(command, args, callback) {
-    var ls    = _spawn.spawn(command, args);
+    var ls = _spawn.spawn(command, args);
 
     console.log("CatJS command:", command, " ", args);
-    
+
     ls.stderr.on('data', function (data) {
         console.log('stderr: ' + data);
     });
@@ -30,11 +30,25 @@ function _call(command, args, callback) {
     });
 }
 
+function _testBegin() {
+    
+    // extract zip file
+    if (!_fs.existsSync(_zipfolder) && _fs.existsSync(_zipfile)) {
+        _admzip = global["$adm-zip"];
 
+        zip = new _admzip(_zipfile);
+        zip.extractAllTo("./test", true);
+    }
+
+    console.log("CatJS running tests from directory: ", process.cwd());
+    
+    // activate catjs core tests
+    _call("node", ["./test/" + _path.join(_extractFolder, "test.js")]);
+}
 
 function _install() {
-    
-    function _downloadTestApps() {
+
+    function _downloadTestApps(callback) {
         _request = global["$request"];
         _requestprogress = global["$request-progress"];
 
@@ -57,15 +71,22 @@ function _install() {
                     console.error("error", err);
                 })
                 .on('close', function (err) {
-                    
-                    process.stdout.write(" Test apps successfully downloaded. ");
+
+                    process.stdout.write("\n Test apps successfully downloaded. ");
 
                     process.stdout.write("\n Start testing CatJS Apps");
-
+                    
+                    if (callback) {
+                        callback.call(this);
+                    }
                 });
+        } else {
+            if (callback) {
+                callback.call(this);
+            }
         }
     }
-    
+
     autonpm("request-progress", {where: path}).then(
         function (mod) {
 
@@ -90,38 +111,57 @@ function _install() {
                         function () {
 
                             var zip;
-                            
+
                             // download test-apps zip file
-                            _downloadTestApps();
-
-                            // extract zip file
-                            if (!_fs.existsSync(_zipfolder) && _fs.existsSync(_zipfile)) {
-                                _admzip = global["$adm-zip"];
-                                
-                                zip = new _admzip(_zipfile);
-                                zip.extractAllTo("./test", false);
-                            }
-
-                            console.log("CatJS running tests from directory: ", process.cwd());
-                            // activate catjs core tests
-                            _call("node", ["./test/" +_path.join(_extractFolder, "test.js")])
+                            _downloadTestApps(function() {
+                                _testBegin();
+                                                               
+                            });
+                         
                         }
-                    );                                       
+                    );
                 }
             );
         });
 }
 
+function _deleteFolderRecursive(path) {
+
+    if (_fs.existsSync(path)) {
+
+        _fs.readdirSync(path).forEach(function (file, index) {
+
+            var curPath = path + "/" + file;
+            if (_fs.lstatSync(curPath).isDirectory()) { // recurse
+
+                _deleteFolderRecursive(curPath);
+            } else { // delete file
+
+                _fs.unlinkSync(curPath);
+            }
+
+        });
+
+        _fs.rmdirSync(path);
+    }
+
+
+}
+
+
 function _clean() {
-        
-    _call('npm', ['rm', 'request'], function() {
-        _call('npm', ['rm', 'request-progress'], function() {
+
+    _call('npm', ['rm', 'request'], function () {
+        _call('npm', ['rm', 'request-progress'], function () {
             process.stdout.write("\n\n CatJS cleanup done!")
-        });        
+        });
     });
 
     if (_fs.existsSync(_zipfile)) {
         _fs.unlinkSync(_zipfile);
+    }
+    if (_fs.existsSync("./test/" + _extractFolder)) {
+        _deleteFolderRecursive("./test/" + _extractFolder);
     }
 }
 
