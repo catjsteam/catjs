68fb0ce6fc680c25f87c7523cd8e14714948f207
arik <lewin.arik@gmail.com>
2014-10-02 17:40:47 +0300

    testdb next implementation

--- src/libraries/cat/core/utils/TestsDB.js
+++ src/libraries/cat/core/utils/TestsDB.js
@@ -1,7 +1,8 @@
 
 _cat.utils.TestsDB = function() {
 
-    var _data;
+    var _data,
+        _testnextcache = {};
 
     (function() {
         _cat.utils.AJAX.sendRequestAsync({
@@ -89,7 +90,7 @@ _cat.utils.TestsDB = function() {
         find : function(field) {
             var code = "JSPath.apply('" + field + "', _data);";
 
-            return new Function("JSPath", "_data", "if (JSPath) { return " + code + "} else { console.log('Missing dependency : JSPath');  }").apply(this, [(typeof JSPath !== "undefined" ? JSPath : undefined), _data]);
+            return (new Function("JSPath", "_data", "if (JSPath) { return " + code + "} else { console.log('Missing dependency : JSPath');  }").apply(this, [(typeof JSPath !== "undefined" ? JSPath : undefined), _data]) || "");
         },
         random: function(field) {
 
@@ -103,6 +104,22 @@ _cat.utils.TestsDB = function() {
             if (result && result.length) {
                 cell = _random(0, result.length-1);
                 return result[cell];
+            }
+
+            return result;
+        },
+        next: function(field) {
+
+            var result = this.find(field),
+                cell=0;
+
+            if (result && result.length) {
+                if (_testnextcache[field] !== undefined && _testnextcache[field] != null) {
+                    _testnextcache[field]++;
+                } else {
+                    _testnextcache[field] = 0;
+                }
+                return result[_testnextcache[field]];
             }
 
             return result;
