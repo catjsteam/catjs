6437bc973f59c598253c76749293b0dcab58cd21
arik <lewin.arik@gmail.com>
2014-11-20 15:13:04 +0200

    (WIP) info modules added

---
+++ src/module/info/Info.js
@@ -0,0 +1,37 @@
+var _JSONUtils = catrequire("cat.jsonutils"),
+    _wrench = require("wrench"),
+    _BaseInfo = require("./Base.js"),
+    _baseinfo = new _BaseInfo();
+
+module.exports = function () {
+
+    return {
+
+        /**
+         * set info to a test entry
+         * 
+         * @param config
+         */
+        set: function (config) {
+                        
+                
+            if (!config) {
+                return undefined;
+            }
+
+            /**
+             * TODO switch case for creating or updating the FS 
+             
+            _baseinfo.createFS({
+                id: "t-b-b",
+                device: "device",
+                type: "android",
+                entity: "info"
+            });
+            */
+            
+        }
+        
+    };
+
+}();
