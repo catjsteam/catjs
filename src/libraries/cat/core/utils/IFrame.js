5c7df9d7163382aa4eceeca836bcbd2823598779
arik <lewin.arik@gmail.com>
2015-04-27 19:22:54 +0300

    iframe ustilities added

--- src/libraries/cat/core/utils/IFrame.js
+++ src/libraries/cat/core/utils/IFrame.js
@@ -1,11 +1,52 @@
 _cat.utils.iframe = function() {
-    return {
-        isIframe : function() {
+    var _module = {
+
+        rootWindow: function() {
+            
+            function _getTopWindow(parentarg) {
+                if (!parentarg) {
+                    parentarg = window;
+                }
+                parentarg = parentarg.parent;
+                if(window.top !== parentarg) {
+                    _getTopWindow(parentarg);
+                }
+                return parentarg;
+            }
+    
+            if(window.top === window.self) {
+    
+                return window.top;
+    
+            } else {
+    
+                return _getTopWindow();
+            }
+        },
+        
+        catroot: function(win) {
+
+            var carroot;
+
+            if (_module.isIframe(win) ){
+                carroot = _module.rootWindow();
+                if (carroot && carroot._cat) {                    
+                    return carroot._cat;
+                }
+            }
+            
+            return undefined;
+        },
+
+        isIframe : function(win) {
+            win = (win || window);
             try {
-                return window.self !== window.top;
+                return win !== win.top;
             } catch (e) {
                 return true;
             }
-        }
+        }                
     };
+    
+    return _module;
 }();
